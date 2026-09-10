3af49b06d693f6cbbcc638f4cf017159568158d0
ishikapandeyy <ishikapandey.workmail@gmail.com>
2026-09-10 11:30:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+	if(s.length==0){
+		return ""
+	}
+	return s.trim().split(" ")[0]
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)); 
+  
